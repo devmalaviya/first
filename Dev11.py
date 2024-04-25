d143e85338ba10d7c24cb96c942d919269048eef
devmalaviya <167281482+devmalaviya@users.noreply.github.com>
2024-04-25 14:23:38 +0530

    Add files via upload

---
+++ Dev11.py
@@ -0,0 +1,36 @@
+# test
+# print name in reverse
+x = "My name is hello"
+print(x[-10:-15:-1])
+print(x[3:8])
+
+# solve error
+x =int(1.2)   #remove""(int,float and str cannot use in one syntax)
+print(x)
+
+# opretors
+# arithmatic opretor
+
+x = 10
+y = 3
+
+print(x+y)  #addition
+print(x-y)  #subtraction
+print(x*y)  #multiplication
+print(x/y)  #float division
+print(x%y)  #modules
+print(x**y) #exponents
+print(x//y) #floor division
+
+# assiment opretor
+x = 5
+
+x +=3
+print(x)
+
+x -=3
+print(x)
+
+x *=3
+print(x)
+
