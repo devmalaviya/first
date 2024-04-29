6d430a96762c4fff53702ea9f4faf6cb89e510d6
devmalaviya <167281482+devmalaviya@users.noreply.github.com>
2024-04-29 13:32:31 +0530

    Add files via upload

---
+++ Dev13.py
@@ -0,0 +1,27 @@
+# identity operator
+
+x = 15
+y =1
+
+print(x is y,x==y)
+print(x is not y,x!= y)
+
+# membership operator
+
+x = "hello"
+print("h" in x)
+
+x = [10,20,30,40]
+print(50 not in x)
+
+# bitwize operator
+# &,|,^
+
+x = 10
+y = 8
+
+print(bin(x))
+print(bin(y))
+print(bin(x&y))
+print(bin(x|y))
+print(bin(x^y))
